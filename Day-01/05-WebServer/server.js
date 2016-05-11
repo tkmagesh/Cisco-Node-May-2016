a3adb97a8c2a62c27641e4cdb2a16dc0c81b9889
tkmagesh <tkmagesh77@gmail.com>
2016-05-11 15:33:24 +0530

    Web Server - Part - 2

--- Day-01/05-WebServer/server.js
+++ Day-01/05-WebServer/server.js
@@ -1,17 +1,30 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
 var server = http.createServer(function(req, res){
 	console.log(req.method, ' - ', req.url);
-	fs.readFile('index.html', {encoding : 'utf8'}, function(err, fileContents){
+	var filename = path.join(__dirname, req.url);
+
+	if (!fs.existsSync(filename)){
+		res.statusCode = 404;
+		res.end();
+	}
+	/*fs.readFile(filename, {encoding : 'utf8'}, function(err, fileContents){
 		if (err){
-			res.statusCode = 404;
+			res.statusCode = 500;
 			res.end();
 			return;
 		}
 		res.write(fileContents);
 		res.end();
-	})
+	})*/
+	var stream = fs.createReadStream(filename);
+	stream.on('error', function(){
+		res.statusCode = 500;
+		res.end();
+	});
+	stream.pipe(res);
 });
 server.listen(8080);
 console.log('server listening on 8080...!');
