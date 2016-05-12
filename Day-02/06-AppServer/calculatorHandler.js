ee3be64d174b3c82d5d96b397567f4f80f51fbe1
tkmagesh <tkmagesh77@gmail.com>
2016-05-12 10:21:31 +0530

    Async Sequencing - Assignment

---
+++ Day-02/06-AppServer/calculatorHandler.js
@@ -0,0 +1,26 @@
+var calculator = require('./calculator');
+var querystring = require('querystring');
+
+module.exports = function(req, res){
+	if (req.data.pathname === '/calculator' && req.method === 'GET'){
+		var data = querystring.parse(req.data.query);
+		var n1 = parseInt(data.n1, 10);
+		var n2 = parseInt(data.n2, 10);
+		var result = calculator[data.op](n1, n2);
+		res.write(result.toString());
+		res.end();
+	} else if (req.data.pathname === '/calculator' && req.method === 'POST'){
+		var reqData = '';
+		req.on('data', function(chunk){
+			reqData += chunk;
+		});
+		req.on('end', function(){
+			var data = querystring.parse(reqData);
+			var n1 = parseInt(data.n1, 10);
+			var n2 = parseInt(data.n2, 10);
+			var result = calculator[data.op](n1, n2);
+			res.write(result.toString());
+			res.end();	
+		});
+	}
+}
---
+++ Day-02/06-AppServer/serveStatic.js
@@ -0,0 +1,36 @@
+var path = require('path'),
+	fs = require('fs');
+
+var staticExtns = ['.html', '.js', '.css', '.xml', '.json', '.png', '.jpg', '.ico', '.txt'];
+function isStatic(extn){
+	return staticExtns.indexOf(extn) !== -1;
+}
+
+
+module.exports = function(req, res){
+	var filename = path.join(__dirname, req.data.pathname);
+	if (isStatic(path.extname(filename))){
+		if (!fs.existsSync(filename)){
+			console.log('serving 404');
+			res.statusCode = 404;
+			res.end();
+		}
+		/*var stream = fs.createReadStream(filename);
+		stream.on('error', function(){
+			res.statusCode = 500;
+			res.end();
+		});
+		stream.pipe(res);*/
+
+		fs.readFile(filename, {encoding : 'utf8'}, function(err, fileContents){
+			if (err){
+				res.statusCode = 500;
+				res.end();
+				return;
+			}
+			console.log('serving file - ', filename);
+			res.write(fileContents);
+			res.end();
+		})
+	}
+} 
